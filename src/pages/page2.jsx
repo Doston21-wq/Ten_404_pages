44e0f8a5173620725a78b1abeb3ac6eae1c762a6
Doston21-wq <rajabovdoston500@gmail.com>
2025-08-10 11:31:16 +0500

    add

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/page404-galaxy.css";
 
 const Page2 = () => {
   return (
-    <div>Page2</div>
-  )
-}
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black overflow-hidden relative">
+      
+      {/* Yulduzlar foni */}
+      <div className="stars"></div>
+      <div className="stars2"></div>
+      <div className="shooting-star"></div>
 
-export default Page2
+      <section className="z-10 text-center px-4">
+        <h1 className="galaxy-404">404</h1>
+        <p className="mt-4 text-lg text-gray-300">
+          Siz izlagan galaktika mavjud emas 🚀
+        </p>
+
+        <Link
+          to="/"
+          className="mt-6 inline-block px-6 py-3 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold shadow-lg hover:scale-105 transform transition"
+        >
+          🌌 Bosh sahifaga qaytish
+        </Link>
+      </section>
+    </main>
+  );
+};
+
+export default Page2;
